2012248b1ae34dbdf177901093ea19b2a8504693
arslan <arslanasghar1565@gmail.com>
2026-07-24 22:25:35 +0500

    revert

--- vulnerable_sample.py
+++
@@ -1,6 +0,0 @@
-# Hardcoded secret
-AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE_SECRET_KEY"
-
-def query_db(user_input):
-    # SQL Injection flaw
-    return f"SELECT * FROM users WHERE username = '{user_input}'"
